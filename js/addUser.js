ea02c22b5bd6022737de03d49e6da135b1eb8869
Diki Armanda <diki.armada5@gmail.com>
2024-09-21 00:57:53 +0700

    feat: sign up and sign in

---
+++ js/addUser.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const userForm = document.getElementById("userForm");
+  const userManager = new User();
+
+  userForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    const userData = {
+      username: document.getElementById("username").value,
+    };
+
+    const result = userManager.saveUser(userData);
+
+    if (result.success) {
+      return window.location.href = "../signin.html";
+    } else {
+      alert("Something went wrong");
+    }
+    
+  });
+
+})
---
+++ js/signin.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const userForm = document.getElementById("userForm");
+  const userManager = new User();
+
+  userForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    const usernameByInput = document.getElementById("username").value;
+
+    const result = userManager.signInUser(usernameByInput);
+
+    if (result.success) {
+      localStorage.setItem("usernameLoggedIn", usernameByInput);
+      return window.location.href = "../tasks.html";
+    } else {
+      alert(result.message);
+    }
+    
+  });
+
+})
